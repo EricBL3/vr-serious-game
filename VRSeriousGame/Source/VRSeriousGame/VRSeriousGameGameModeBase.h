49803df329b65f79e30a7273c7416331de28d73c
EricBL3 <ericbuitron3@gmail.com>
2022-10-05 12:51:03 -0500

    Create Unreal Project

---
+++ VRSeriousGame/Source/VRSeriousGame/VRSeriousGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRSeriousGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRSERIOUSGAME_API AVRSeriousGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
